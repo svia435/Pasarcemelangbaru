271bddc2e49b028cfd77dbeeb1f3ae9cd3a4b4cb
svia435 <svia435@gmail.com>
2023-10-09 11:02:11 +0700

    [WIP] menambahkan konfigurasi

---
+++ konfigurasi-firebase.js
@@ -0,0 +1,8 @@
+export const konfigurasiFirebase = {
+    apiKey: "AIzaSyCRlnLBhUHGOF7Lfg9iy_SbfK6coM_7f1U",
+    authDomain: "insan-cemerlang-6640c.firebaseapp.com",
+    projectId: "insan-cemerlang-6640c",
+    storageBucket: "insan-cemerlang-6640c.appspot.com",
+    messagingSenderId: "917464283158",
+    appId: "1:917464283158:web:3a6179cd71818d68f6dd37"
+  };
